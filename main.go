0276da78aef8e2eca95bea249240cde1e30ce1c5
Rich Abdill <richard.abdill@gmail.com>
2017-10-22 01:20:24 -0500

    Infinite loop for typing a target

--- main.go
+++ main.go
@@ -2,22 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
+	"regexp"
+	"strings"
 )
+
+func processTarget(input []byte) (output string) {
+	output = string(input)
+	output = strings.ToLower(output)
+
+	// cut out line breaks:
+	output = strings.Replace(output, "\n", " ", -1)
+	// make sure we don't have long stretches of spaces
+	re := regexp.MustCompile(" +")
+	output = re.ReplaceAllLiteralString(output, " ")
+	// make sure we only have alphabet characters now:
+	re = regexp.MustCompile("[^a-z ]")
+	output = re.ReplaceAllLiteralString(output, "")
+	return
+}
 
 func main() {
 	rand.Seed(9)
-	possibilities := "abcdefghij"
-	target := "abcdefghij"
+	possibilities := "abcdefghijklmnopqrstuvqxyz    "
+
+	file, err := ioutil.ReadFile("target.txt")
+	if err != nil {
+		fmt.Printf("\n\nERROR=======\n|%v|\n\n", err)
+		os.Exit(1)
+	}
+	target := processTarget(file)
 
 	currentPosition := 0
 	highwater := -1
 
-	for i := 0; i < 99999999; i++ {
+	for true {
 		keyPress := possibilities[rand.Intn(len(possibilities))]
 		if keyPress == target[currentPosition] {
 			if currentPosition > highwater {
-				fmt.Print(keyPress)
+				fmt.Print(target[:currentPosition+1])
 				fmt.Printf("\nNEW HIGH POINT: was %v, now %v\n", highwater, currentPosition)
 				highwater = currentPosition
 			}
@@ -28,9 +53,10 @@ func main() {
 			currentPosition = 0
 		}
 	}
-	fmt.Printf("\n\nCOMPLETE!\nLongest achievement: ")
-	for p := 0; p <= highwater; p++ {
-		fmt.Print(string(target[p]))
-	}
-	fmt.Print("\n\n")
+	// This section is unreachable when the for loop is infinite:
+	// fmt.Printf("\n\nCOMPLETE!\nLongest achievement: ")
+	// for p := 0; p <= highwater; p++ {
+	// 	fmt.Print(string(target[p]))
+	// }
+	// fmt.Print("\n\n")
 }

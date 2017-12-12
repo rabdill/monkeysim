06c33132822bb2be922f906abfa378510805c8e4
Rich Abdill <richard.abdill@gmail.com>
2017-12-11 23:46:16 -0600

    Client doesn't keep hitting dead server

--- static/builder.js
+++ static/builder.js
@@ -1,34 +1,30 @@
-function get (url, cb) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() { 
-        if (req.readyState == 4 && req.status == 200)
-            cb(req.responseText);
-    }
-    req.open("GET", url, true);            
-    req.send(null);
+function get(url) {
+    return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+            if(req.readyState == 4) {
+                if(req.status == 200) resolve(req.responseText);
+                else reject(req);
+            }
+        }
+        req.open("GET", url, true);            
+        req.send(null);
+    });
 };
 
 function updateMonkeys() {
-    get('/info', function(response) {
+    get('/info').then(function(response) {
         monkeys = JSON.parse(response)
-        console.log(monkeys);
-        console.log(monkeys.length);
         guts = "";
         for(var i=0, monkey; monkey = monkeys[i]; i++) {
             guts += "<li><strong>" + monkey.Name + "</strong> (" + monkey.Speed.toFixed(3) + " kkps): " + monkey.Progress;
         }
         document.getElementById("results").innerHTML = guts;
+
+        setTimeout(updateMonkeys, 2000);
+    }, function(err) {
+        console.log("ERROR, bailing on requests: ", err);
     });
 };
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
-async function keepGoing() {
-    while(true) {
-        updateMonkeys();
-        await sleep(2000);
-    }
-};
-
-keepGoing();
+updateMonkeys();
